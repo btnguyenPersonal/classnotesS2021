83590bc33183a6634b2037e02b7f2aee0d5da2d5
btnguyenPersonal <btnguyenPersonal@github.com>
2022-02-18 23:26:37 -0600

    363 database set up, need to insert into tables now

--- coms363/btnguyenQ1a.sql
+++ coms363/btnguyenQ1a.sql
@@ -2,29 +2,30 @@ drop database if exists btnguyen;
 create database btnguyen;
 use btnguyen;
 
-drop table if exists Dependents;
-create table Dependents (
-	dname varchar(255),
-	relationships varchar(255),
-    date_of_birth date,
-    primary key (dname)
-);
-
 drop table if exists Employees;
 create table Employees (
-	essn int,
+	essn int not null,
     ename varchar(255),
     eaddress varchar(255),
     esalary varchar(255),
     ephone_num varchar(10),
-    union_mem_num int,
-    dname varchar(255),
+    union_mem_num int unique,
     primary key (essn)
+);
+
+drop table if exists Dependents;
+create table Dependents (
+	dname varchar(255) not null,
+	relationships varchar(255),
+    date_of_birth date,
+    dependssn int unique not null,
+    primary key (dname, dependssn),
+    foreign key (dependssn) references Employees (essn)
 );
 
 drop table if exists Model;
 create table Model (
-	model_no int,
+	model_no int not null,
     capacity int,
     weigth int,
     primary key (model_no)
@@ -33,52 +34,77 @@ create table Model (
 drop table if exists Technician;
 create table Technician (
 	security_clearance varchar(255),
-    us_passport_no int
+	us_passport_no int,
+	tech_ssn int primary key,
+    foreign key (tech_ssn) references Employees (essn)
 );
+
+drop table if exists Expert_of;
+create table Expert_of (
+	model_no int not null,
+	t_ssn int not null,
+    foreign key (model_no) references Model (model_no),
+    primary key (t_ssn, model_no)
+);
+
+alter table Expert_of
+add foreign key (t_ssn) references Technician (tech_ssn);
 
 drop table if exists Manager;
 create table Manager (
-	department varchar(255)
+	department varchar(255),
+    manager_ssn int primary key,
+    foreign key (manager_ssn) references Employees (essn)
 );
 
 drop table if exists Traffic_Control;
 create table Traffic_Control (
-	exam_date date
+	exam_date date,
+    traffic_ssn int primary key,
+    foreign key (traffic_ssn) references Employees (essn)
 );
 
 drop table if exists Plane;
 create table Plane (
-	reg_no int,
+	reg_no int not null,
     pname varchar(255),
-    primary key (reg_no)
+    ptype int not null,
+    primary key (reg_no),
+    foreign key (ptype) references Model (model_no)
 );
 
 drop table if exists Test;
 create table Test (
+    FAA_no int not null,
 	score int,
     tname varchar(255),
-    FAA_no int,
     primary key(FAA_no)
 );
 
--- alter table Dependents 
--- add constraint Depends foreign key (essn)
--- references Employees (essn);	
+drop table if exists Test_info;
+create table Test_info (
+	hours int,
+    score int,
+    tdate date,
+    tech_ssn int not null,
+    reg_no int not null,
+    FAA_no int not null,
+    foreign key (tech_ssn) references Technician (tech_ssn),
+    foreign key (reg_no) references Plane (reg_no),
+    foreign key (FAA_no) references Test (FAA_no),
+    primary key (tech_ssn, reg_no, FAA_no)
+);
 
--- alter table Dependents
--- add constraint must_have_only_one unique (essn);
-
-alter table Employees 
-add constraint Depends foreign key (dname) 
-references Dependents (dname);	
-
--- alter table Employees 
--- add constraint ISA foreign key (dname) 
--- references Dependents (dname);	
-
--- alter table Dependents 
--- add foreign key (essn) 
--- references Employees (essn);
-
--- alter table Dependents 
--- add constraint unique (essn);				
+drop table if exists Monitor;
+create table Monitor (
+	most_recent_date date,
+    manager_ssn int not null,
+    tech_ssn int not null,
+    reg_no int not null,
+    FAA_no int not null,
+    foreign key (tech_ssn) references Test_info (tech_ssn),
+    foreign key (reg_no) references Test_info (reg_no),
+    foreign key (FAA_no) references Test_info (FAA_no),
+    foreign key (manager_ssn) references Manager (manager_ssn),
+    primary key (tech_ssn, reg_no, FAA_no, manager_ssn)
+);
---
+++ coms363/btnguyenQ1b.sql
@@ -0,0 +1,14 @@
+use btnguyen;
+delete from Employees where true;
+insert into Employees
+(essn, ename, eaddress, esalary, ephone_num, union_mem_num)
+values (0, "Ben", "1732 46th street", 1000000, "1234561234", 1);
+insert into Employees
+(essn, ename, eaddress, esalary, ephone_num, union_mem_num)
+values (1, "Josh", "100 49th street", 2000000, "8484848484", 2);
+insert into Employees
+(essn, ename, eaddress, esalary, ephone_num, union_mem_num)
+values (2, "Selma", "202 2nd street", 500000, "9999999999", 3);
+insert into Employees
+(essn, ename, eaddress, esalary, ephone_num, union_mem_num)
+values (3, "Coby", "355 lincoln way", 4000, "3334441234", 4);
