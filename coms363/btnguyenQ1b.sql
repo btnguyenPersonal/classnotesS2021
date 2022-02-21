1aa86eea488e388a33d9a9e94fe7731dc5c64cac
btnguyenPersonal <btnguyenPersonal@github.com>
2022-02-20 19:39:36 -0600

    363 1b done

--- coms363/btnguyenQ1b.sql
+++ coms363/btnguyenQ1b.sql
@@ -1,7 +1,17 @@
 use btnguyen;
+delete from Monitor where true;
+delete from Test_info where true;
+delete from Expert_of where true;
+delete from Technician where true;
+delete from Test where true;
+delete from Plane where true;
+delete from Manager where true;
+delete from Model where true;
+delete from Dependents where true;
+delete from Traffic_Control where true;
+delete from Employees where true;
 
 -- Employees Table
-delete from Employees where true;
 insert into Employees (essn, ename, eaddress, esalary, ephone_num, union_mem_num)
 values (0, "Ben", "1732 46th street", 1000000, "1234561234", 1);
 insert into Employees (essn, ename, eaddress, esalary, ephone_num, union_mem_num)
@@ -28,7 +38,6 @@ insert into Employees (essn, ename, eaddress, esalary, ephone_num, union_mem_num
 values (11, "Dylan", "100 Freddy Court", 10, "1923812377", 12);
 
 -- Dependents Table
-delete from Dependents where true;
 insert into Dependents (dname, relationships, date_of_birth, dependssn)
 values ("Batman", "Brother", "1985-01-02", 0);
 insert into Dependents (dname, relationships, date_of_birth, dependssn)
@@ -39,7 +48,6 @@ insert into Dependents (dname, relationships, date_of_birth, dependssn)
 values ("Wonder Woman", "Mom", "2000-02-05", 3);
 
 -- Traffic_Control Table
-delete from Traffic_Control where true;
 insert into Traffic_Control (exam_date, traffic_ssn)
 values ("2011-02-03", 0);
 insert into Traffic_Control (exam_date, traffic_ssn)
@@ -50,7 +58,6 @@ insert into Traffic_Control (exam_date, traffic_ssn)
 values ("2010-05-02", 3);
 
 -- Model Table
-delete from Model where true;
 insert into Model (model_no, capacity, weight)
 values (0, 50, 10000);
 insert into Model (model_no, capacity, weight)
@@ -61,7 +68,6 @@ insert into Model (model_no, capacity, weight)
 values (3, 1000, 1000000);
 
 -- Manager Table
-delete from Manager where true;
 insert into Manager (department, manager_ssn)
 values ("Chicago", 4);
 insert into Manager (department, manager_ssn)
@@ -72,7 +78,6 @@ insert into Manager (department, manager_ssn)
 values ("Ames", 7);
 
 -- Plane Table
-delete from Plane where true;
 insert into Plane (reg_no, pname, ptype)
 values (0, "Blackbird", 0);
 insert into Plane (reg_no, pname, ptype)
@@ -83,7 +88,6 @@ insert into Plane (reg_no, pname, ptype)
 values (3, "Bomber", 3);
 
 -- Test Table
-delete from Test where true;
 insert into Test (FAA_no, score, tname)
 values (0, 10, "Landing");
 insert into Test (FAA_no, score, tname)
@@ -91,48 +95,45 @@ values (1, 9, "Nose Dive");
 insert into Test (FAA_no, score, tname)
 values (2, 7, "Barrel Roll");
 insert into Test (FAA_no, score, tname)
-values (3, 4, "Liftoff");
-
--- Test_info Table
-delete from Test_info where true;
-insert into Test_info (FAA_no, score, tname)
-values (0, 10, "Landing");
-insert into Test_info (FAA_no, score, tname)
-values (1, 9, "Nose Dive");
-insert into Test_info (FAA_no, score, tname)
-values (2, 7, "Barrel Roll");
-insert into Test_info (FAA_no, score, tname)
 values (3, 4, "Liftoff");
 
 -- Technician Table
-delete from Technician where true;
-insert into Technician (FAA_no, score, tname)
-values (0, 10, "Landing");
-insert into Technician (FAA_no, score, tname)
-values (1, 9, "Nose Dive");
-insert into Technician (FAA_no, score, tname)
-values (2, 7, "Barrel Roll");
-insert into Technician (FAA_no, score, tname)
-values (3, 4, "Liftoff");
-
--- Monitor Table
-delete from Monitor where true;
-insert into Monitor (FAA_no, score, tname)
-values (0, 10, "Landing");
-insert into Monitor (FAA_no, score, tname)
-values (1, 9, "Nose Dive");
-insert into Monitor (FAA_no, score, tname)
-values (2, 7, "Barrel Roll");
-insert into Monitor (FAA_no, score, tname)
-values (3, 4, "Liftoff");
+insert into Technician (security_clearance, us_passport_no, tech_ssn)
+values ("no clearance", 4999, 8);
+insert into Technician (security_clearance, us_passport_no, tech_ssn)
+values ("level 1 clearance", 1923333, 9);
+insert into Technician (security_clearance, us_passport_no, tech_ssn)
+values ("super secret access", 12372138, 10);
+insert into Technician (security_clearance, us_passport_no, tech_ssn)
+values ("all access", 123747, 11);
 
 -- Expert_of Table
-delete from Expert_of where true;
-insert into Expert_of (FAA_no, score, tname)
-values (0, 10, "Landing");
-insert into Expert_of (FAA_no, score, tname)
-values (1, 9, "Nose Dive");
-insert into Expert_of (FAA_no, score, tname)
-values (2, 7, "Barrel Roll");
-insert into Expert_of (FAA_no, score, tname)
-values (3, 4, "Liftoff");
+insert into Expert_of (model_no, t_ssn)
+values (0, 8);
+insert into Expert_of (model_no, t_ssn)
+values (1, 9);
+insert into Expert_of (model_no, t_ssn)
+values (2, 10);
+insert into Expert_of (model_no, t_ssn)
+values (3, 11);
+
+-- Test_info Table
+insert into Test_info (hours, score, tdate, tech_ssn, reg_no, FAA_no)
+values (40, 10, "2010-01-02", 8, 0, 0);
+insert into Test_info (hours, score, tdate, tech_ssn, reg_no, FAA_no)
+values (1, 1, "2000-02-19", 9, 1, 1);
+insert into Test_info (hours, score, tdate, tech_ssn, reg_no, FAA_no)
+values (10, 4, "2020-04-19", 10, 2, 2);
+insert into Test_info (hours, score, tdate, tech_ssn, reg_no, FAA_no)
+values (2, 8, "2021-05-01", 11, 3, 3);
+
+-- Monitor Table
+insert into Monitor (most_recent_date, manager_ssn, tech_ssn, reg_no, FAA_no)
+values ("2020-02-10", 4, 8, 0, 0);
+insert into Monitor (most_recent_date, manager_ssn, tech_ssn, reg_no, FAA_no)
+values ("2020-03-05", 5, 9, 1, 1);
+insert into Monitor (most_recent_date, manager_ssn, tech_ssn, reg_no, FAA_no)
+values ("2020-10-10", 6, 10, 2, 2);
+insert into Monitor (most_recent_date, manager_ssn, tech_ssn, reg_no, FAA_no)
+values ("2020-12-25", 7, 11, 3, 3);
+
